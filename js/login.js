13b9b9a5ec1a61ac827693b74220acfb6eb3d9b4
Alexey <70903840+Imonahorse@users.noreply.github.com>
2020-10-26 19:37:39 +0300

    Добавил js для попапа напишите нам

---
+++ js/login.js
@@ -0,0 +1,57 @@
+// Открытие модального окна на главной странице
+const loginLink = document.querySelector(".map-help");
+const loginPopup = document.querySelector(".modal-login");
+const loginClose = loginPopup.querySelector(".modal-close");
+const loginLogin = loginPopup.querySelector(".modal-input-login");
+const loginPassword = loginPopup.querySelector(".modal-input-password");
+const loginForm = loginPopup.querySelector(".modal-form");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginPassword.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginPassword.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+      loginPopup.classList.remove("modal-error");
+    }
+  }
+});
