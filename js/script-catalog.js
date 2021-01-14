6e3f01c73186f4ff0ab939b573e581c110c1cc36
Alexey <Awotjan@gmail.com>
2021-01-14 11:46:19 +0300

    demo

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -5,19 +5,20 @@ const catalogModalClose = catalogModal.querySelector(".catalog-modal-close");
 const modalFirstButton = catalogModal.querySelector(".catalog-ordering")
 
 
-  for (var i = 0; i < addToBasket.length; i++) {
-    addToBasket[i].addEventListener("click", function (evt) {
+for (var i = 0; i < addToBasket.length; i++) {
+  addToBasket[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     catalogModal.classList.add("modal-show");
     modalFirstButton.focus();
     console.log("клик")
-    });
-  };
-
-  catalogModalClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    catalogModal.classList.remove("modal-show");
   });
+}
+;
+
+catalogModalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  catalogModal.classList.remove("modal-show");
+});
 
 
 
